2d4dc002b53956775582536cfc03f807beef255b
Suraj Kumar <sk658139@gmail.com>
2025-04-22 11:35:54 +0530

    added functionalities  of the app

--- app/src/main/java/com/surajvanshsv/sportsapp/MainActivity.java
+++ app/src/main/java/com/surajvanshsv/sportsapp/MainActivity.java
@@ -7,14 +7,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // 1- Adapter View
+    private RecyclerView recyclerView;
+
+    // 2- Data Source
+    private List<Sport> sportList;
+
+    // 3- Adapter
+    private CustomAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        sportList = new ArrayList<>();
+
+        Sport s1 = new Sport("Football", R.drawable.football);
+        Sport s2 = new Sport("Basketball", R.drawable.basketball);
+        Sport s3 = new Sport("VolleyBall", R.drawable.volley);
+        Sport s4 = new Sport("Tennis", R.drawable.tennis);
+        Sport s5 = new Sport("Ping Pong", R.drawable.ping);
+
+        sportList.add(s1);
+        sportList.add(s2);
+        sportList.add(s3);
+        sportList.add(s4);
+        sportList.add(s5);
+
+
+        myAdapter = new CustomAdapter(sportList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
